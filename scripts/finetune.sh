a4eb89d78665b80c9d47bd7e9c7a7987780bf1a4
Phuc3010 <marksmans2002@gmail.com>
2024-03-09 14:39:00 +0000

    Add new pref scan scripts

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -1,1 +1,2 @@
-ACCELERATE_LOG_LEVEL=info accelerate launch --config_file config/multi_gpu.yaml --num_processes=8 --main_process_port 2950 src/run_kto.py config/config.yaml
+export CUDA_VISIBLE_DEVICES="0, 4"
+ACCELERATE_LOG_LEVEL=info accelerate launch --config_file config/deepspeed_zero2.yaml --num_processes=2 --main_process_port 2950 src/run_kto.py config/config.yaml
--- scripts/pref_scan/finetune_scan.sh
+++ scripts/pref_scan/finetune_scan.sh
@@ -1,18 +1,18 @@
-export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+export CUDA_VISIBLE_DEVICES="4,5"
 
 #!/bin/bash
-priors=("0.1" "0.2" "0.3" "0.4" "0.5" "0.6" "0.7" "0.8" "0.9")
+priors=("0.1" "0.2" "0.3" "0.4" "0.5" "0.6" "0.7")
 epochs=("1" "2")
-SCRIPT_COMMAND="ACCELERATE_LOG_LEVEL=info accelerate launch --config_file config/deepspeed_zero3.yaml --num_processes=8 --main_process_port 2950 src/run_kto.py config/config.yaml --prior="
+SCRIPT_COMMAND="ACCELERATE_LOG_LEVEL=info accelerate launch --config_file config/deepspeed_zero2.yaml --num_processes=2 --main_process_port 2950 src/run_kto.py config/config.yaml --prior="
 
 # Loop through different prior values
 for epoch in "${epochs[@]}"; do
   for prior in "${priors[@]}"; do
     # Run the script with the current prior value
-    OUTPUT_DIR="data/model/zephyr-7b-kto-iter0-${prior}-${epoch}"
-    EVAL_PATH="data/output/zephyr-7b-kto-iter0-${prior}-${epoch}"
+    OUTPUT_DIR="data/model/tiny-llama-kto-iter0-${prior}-${epoch}"
+    EVAL_PATH="data/output/tiny-llama-kto-iter0-${prior}-${epoch}"
     
-    echo "$SCRIPT_COMMAND$prior --num_train_epochs=${epoch} --output_dir=${OUTPUT_DIR} --hub_model_id=zephyr-7b-kto-iter0-${prior}-epoch${epoch}"
+    echo "$SCRIPT_COMMAND$prior --num_train_epochs=${epoch} --output_dir=${OUTPUT_DIR} --hub_model_id=tiny-llama-kto-iter0-${prior}-epoch${epoch}"
 
     eval "accelerate launch -m lm_eval --model=hf --model_args pretrained=${OUTPUT_DIR},dtype=bfloat16 --tasks gsm8k --batch_size 1 --num_fewshot 5 --output_path ${EVAL_PATH}/gsm8k  --log_samples"
     eval "accelerate launch -m lm_eval --model=hf --model_args pretrained=${OUTPUT_DIR},dtype=bfloat16 --tasks hellaswag --batch_size 1 --num_fewshot 10 --output_path ${EVAL_PATH}/hellaswag --log_samples"
